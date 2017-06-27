2ca1bb3709060df9218f241ac02b087ea766f4c4
Rolfjdra <rolfj.dramdal@gmail.com>
2017-06-27 14:14:27 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -104,6 +104,7 @@ app.post('/webhook', (req, res) => {
         sessionId, // aktiv session
         msg, // the user's message 
         sessions[sessionId].context, // session state
+		sendGenericMessage(sender),
         (error, context) => {
           if (error) {
             console.log('Oops! Fikk en feil fra Wit:', error);
