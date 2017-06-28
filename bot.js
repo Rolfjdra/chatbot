99e1d9dda43c45934072ffa1156ffd9ed9562e26
Rolfjdra <rolfj.dramdal@gmail.com>
2017-06-28 14:21:46 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -79,7 +79,7 @@ const actions = {
 				    "image_url": "https://dfo.no/Images/logo_dfo.png",
 				    "buttons": [{
 					    "type": "web_url",
-					    "url": wantedLinks2,
+					    "url": "wantedLinks2",
 					    "title": "web url"
 				    }, {
 					    "type": "postback",
