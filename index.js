324befe7cd4c67a561020280e953fb8a2fa0fe1c
Rolfjdra <rolfj.dramdal@gmail.com>
2017-06-28 13:05:28 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -113,10 +113,10 @@ app.post('/webhook', (req, res) => {
             // bot er ferdig
             // Venter på mer input
             console.log('Venter på meldinger');
-			const messageData2 = bot.GenericMessage(messageData);
+			
 			// oppdater session state
             sessions[sessionId].context = context;
-			sendGenericMessage(sender, messageData2)
+			sendGenericMessage(sender)
             // Reset session?
             // Kanskje med annen logikk..
             // Eksempel: Prøver med "intent"
@@ -132,14 +132,36 @@ app.post('/webhook', (req, res) => {
   res.sendStatus(200);
 });
 
-function sendGenericMessage(sender, messageData){
-	    request({
+function sendGenericMessage(sender) {
+    let messageData = {
+	    "attachment": {
+		    "type": "template",
+		    "payload": {
+				"template_type": "generic",
+			    "elements": [{
+					"title": "DFO",
+				    "subtitle": "Brukerveiledning",
+				    "image_url": "https://dfo.no/Images/logo_dfo.png",
+				    "buttons": [{
+					    "type": "web_url",
+					    "url": "https://dfo.no/kundesider/lonnstjenester/selvbetjening/stottede-nettlesere/",
+					    "title": "web url"
+				    }, {
+					    "type": "postback",
+					    "title": "Postback",
+					    "payload": "Payload for first element in a generic bubble",
+				    }]
+			    }]
+		    }
+	    }
+    }
+    request({
 	    url: 'https://graph.facebook.com/v2.6/me/messages',
 	    qs: {access_token:token},
 	    method: 'POST',
 	    json: {
 		    recipient: {id:sender},
-		    message: messageData2,
+		    message: messageData,
 	    }
     }, function(error, response, body) {
 	    if (error) {
@@ -148,5 +170,4 @@ function sendGenericMessage(sender, messageData){
 		    console.log('Error: ', response.body.error)
 	    }
     })
-
 }
