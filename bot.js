d3dc781b2a4f7506ea72c1f6aded7ffebbc2b8e3
Rolfjdra <rolfj.dramdal@gmail.com>
2017-06-29 08:42:33 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -68,7 +68,7 @@ const actions = {
     if (category) {
       context.cat = category; // lagrer i context
 	  const wantedLinks2 = allLinks[context.cat || 'default']
-	  var myLink = "{"url": "wantedLinks2"}";
+	  var myLink = {"url": "wantedLinks2"};
 	  const jsonlink = JSON.stringify(myLink);
 	  const messageData = {
 	    "attachment": {
