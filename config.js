456cf45279221ae42fec3db3784b97cb69353c93
Rolfjdra <rolfj.dramdal@gmail.com>
2017-06-30 10:42:49 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ if (!WIT_TOKEN) {
 }
 
 
-var FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN || "EAABcpUgvhNoBAEsZCLChENftlNaMmzS9bF69YfKqiGSH7m4ZAZCsmvdGmkfNEymmPuzKmhuHLPFePwwnfWUyZCdIQpttqydZAr1eUYAsrD333rLDWKvlyxipms7CMCLE7ZAvwiFZAzKFofGZBWsZAunBmCmEuDZCjMkZANURmzDGhLZCJ6ZAZBmbZCNo8hrBf93pr9PCfoZD";
+var FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN || "EAABcpUgvhNoBAI7GE4Tf6VTF2xayOnIUxBBBhM3DUveW2kKQDByPtGx4KEe8UxxCe4DK2q71qTJoALgYZCIj0odZC5UMZBamshWeywQ6LWfSXIhpJ9OOazeK0V8ZABzFZAXL6VwnDLSGnZCpP7UOZCabw1FDGhdgXQw9ZCyZBvRyBo03hxoxMmFVF7oo4Dg8FRaAZD";
 if (!FB_PAGE_TOKEN) {
 	throw new Error("Ingen FB_PAGE_TOKEN. Gå til https://developers.facebook.com/docs/pages/access-tokens")
 }
