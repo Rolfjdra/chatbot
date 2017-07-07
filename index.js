e685f0624f85107650bd0296ecfda3f7a3b2beb6
Rolfjdra <rolfj.dramdal@gmail.com>
2017-07-07 10:04:01 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -108,12 +108,12 @@ app.post('/webhook', (req, res) => {
         (error, context) => {
           if (error) {
             console.log('Oops! Fikk en feil fra Wit:', error);
+		  }
 		  else if (msg == "hei" || msg == "hallo" || msg == "heisann" || msg == "hjelp") {
 			  let messageData = bot.startmsg;
 			  sendGenericMessage(sender,messageData)
 			  delete sessions[sessionId];
-			  messageData = undefined;
-		  }			  
+			  messageData = undefined;		  
           } else {
             // bot er ferdig
             // Venter på mer input
