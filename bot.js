fc5f033baee4ce921c6dc293327e4d857aa0b167
Rolfjdra <rolfj.dramdal@gmail.com>
2017-07-19 10:01:09 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -673,7 +673,7 @@ const actions = {
 
 
 const getWit = () => {
-  return new Wit(Config.WIT_TOKEN, actions);
+  return new Wit({ accessToken: Config.WIT_TOKEN, actions:actions });
 };
 
 exports.getWit = getWit;
