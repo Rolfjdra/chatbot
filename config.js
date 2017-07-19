7dc57a7cfc7bd199d8673cb37c1fd4f6f28fa41d
Rolfjdra <rolfj.dramdal@gmail.com>
2017-07-19 14:30:48 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 'use strict';
 // Henter opp vertifikasjoner -> Facebookside, WIT-api, Heroku-webhook
 // tester dfoface2
-const WIT_TOKEN = process.env.WIT_TOKEN || "WRAXCGEZKGZCMMAHKGDQGKBHQZUB273X"
+const WIT_TOKEN = process.env.WIT_TOKEN || "IIM3YKRT7YNJ5KR72YUU2R5J5P4JYBGX"
 if (!WIT_TOKEN) {
   throw new Error("Ingen WIT_TOKEN. Hent: https://wit.ai/")
 }
 
 
-var FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN || "EAACtPl97H00BANw3vkyiALfImKBmY3FYNKXvBO7j9xNHZAEHVAYiMIYFZA00qbyvN8NxZCPY6dTcCYDHdDayjfGwVTkj9QSVZCyO7S3J6iu2uWlIUNpGWni2pcZCOuqpGQejKDn4lA4hymzj3BxDCZBq7KcgwGN5aqzPsRmjwq7QZDZD";
+var FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN || "EAACtPl97H00BALSImxYPCAtr4AJDUzsEwaNlZBdoei78JBP4zs2nkSCKiLeFNcEgWX5jjTBZCLssh6ffiIbupgH5X5FdRJi2Eth4IvjZCn5bN5z6LcWZALwA6NCcqUgIqE1UFxfzp9TuR4l0bP6EPOjCDVYIXqgrglIAkdcYCwZDZD";
 if (!FB_PAGE_TOKEN) {
 	throw new Error("Ingen FB_PAGE_TOKEN. Gå til https://developers.facebook.com/docs/pages/access-tokens")
 }
