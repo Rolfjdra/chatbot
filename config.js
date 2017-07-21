08488eff97d86980f76e0cad47d572451fe4823a
Rolfjdra <rolfj.dramdal@gmail.com>
2017-07-21 13:44:30 +0200

    Add files via upload
    Hide config vars

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 'use strict';
 // Henter opp vertifikasjoner -> Facebookside, WIT-api, Heroku-webhook
 // tester dfoface2
-const WIT_TOKEN = process.env.WIT_TOKEN || "I6HR6LS3WXUKUCXUGRINCJBRQ2TBTGK7"
+const WIT_TOKEN = process.env.WIT_TOKEN
 if (!WIT_TOKEN) {
   throw new Error("Ingen WIT_TOKEN. Hent: https://wit.ai/")
 }
 
 
-var FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN || "EAACtPl97H00BALSImxYPCAtr4AJDUzsEwaNlZBdoei78JBP4zs2nkSCKiLeFNcEgWX5jjTBZCLssh6ffiIbupgH5X5FdRJi2Eth4IvjZCn5bN5z6LcWZALwA6NCcqUgIqE1UFxfzp9TuR4l0bP6EPOjCDVYIXqgrglIAkdcYCwZDZD";
+var FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN
 if (!FB_PAGE_TOKEN) {
 	throw new Error("Ingen FB_PAGE_TOKEN. Gå til https://developers.facebook.com/docs/pages/access-tokens")
 }
 
-var FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN || 'rolf'
+var FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN
 
 module.exports = {
   WIT_TOKEN: WIT_TOKEN,
